81c42e80fa34492b015dba7cc56a14f27de3cd23
nahidn228 <nahidn228@gmail.com>
2025-10-13 10:06:44 +0600

    Uploading Image to Cloudinary and create patient

--- src/app/helper/fileUploader.ts
+++ src/app/helper/fileUploader.ts
@@ -1,6 +1,7 @@
 import multer from "multer";
 import path from "path";
 import { v2 as cloudinary } from "cloudinary";
+import config from "../../config";
 
 //multer
 const storage = multer.diskStorage({
@@ -18,10 +19,72 @@ const upload = multer({ storage: storage });
 //cloudinary
 
 const uploadToCloudinary = async (file: Express.Multer.File) => {
-  console.log("File", file);
+  // Configuration
+  cloudinary.config({
+    cloud_name: config.cloudinary.cloud_name,
+    api_key: config.cloudinary.api_key,
+    api_secret: config.cloudinary.api_secret,
+  });
+
+  // Upload an image
+  const uploadResult = await cloudinary.uploader
+    .upload(file.path, {
+      public_id: file.filename,
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  return uploadResult;
 };
+
+
 
 export const fileUploader = {
   upload,
   uploadToCloudinary,
 };
+
+
+
+
+
+// (async function() {
+
+//     // Configuration
+//     cloudinary.config({
+//         cloud_name: ,
+//         api_key: ,
+//         api_secret: '<your_api_secret>' 
+//     });
+
+//     // Upload an image
+//      const uploadResult = await cloudinary.uploader
+//        .upload(
+//            'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
+//                public_id: 'shoes',
+//            }
+//        )
+//        .catch((error) => {
+//            console.log(error);
+//        });
+
+//     console.log(uploadResult);
+
+//     // Optimize delivery by resizing and applying auto-format and auto-quality
+//     const optimizeUrl = cloudinary.url('shoes', {
+//         fetch_format: 'auto',
+//         quality: 'auto'
+//     });
+
+//     console.log(optimizeUrl);
+
+//     // Transform the image: auto-crop to square aspect_ratio
+//     const autoCropUrl = cloudinary.url('shoes', {
+//         crop: 'auto',
+//         gravity: 'auto',
+//         width: 500,
+//         height: 500,
+//     });
+
+//     console.log(autoCropUrl);
+// })();
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -8,33 +8,29 @@ import { fileUploader } from "../../helper/fileUploader";
 const createPatient = async (req: Request) => {
   if (req.file) {
     const uploadedResult = await fileUploader.uploadToCloudinary(req.file);
-    console.log(uploadedResult);
+    req.body.patient.profilePhoto = uploadedResult?.secure_url;
   }
 
-  // const hashedPassword = await bcrypt.hash(
-  //   req.body.password,
-  //   Number(config.salt_round)
-  // );
+  const hashedPassword = await bcrypt.hash(
+    req.body.password,
+    Number(config.salt_round)
+  );
 
-  // const result = await prisma.$transaction(async (tx) => {
-  //   await tx.user.create({
-  //     data: {
-  //       email: req.body.email,
-  //       password: hashedPassword,
-  //       name: req.body.name,
-  //     },
-  //   });
+  const result = await prisma.$transaction(async (tx) => {
+    await tx.user.create({
+      data: {
+        email: req.body.patient.email,
+        password: hashedPassword,
+        name: req.body.patient.name,
+      },
+    });
 
-  //   return await tx.patient.create({
-  //     data: {
-  //       email: req.body.email,
-  //       password: hashedPassword,
-  //       name: req.body.name,
-  //     },
-  //   });
-  // });
+    return await tx.patient.create({
+      data: req.body.patient,
+    });
+  });
 
-  // return result;
+  return result;
 };
 
 export const UserService = {
