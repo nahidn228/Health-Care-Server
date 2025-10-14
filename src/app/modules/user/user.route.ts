2e66cdac2d426300764b941ecec4a2def3380e8a
nahidn228 <nahidn228@gmail.com>
2025-10-14 11:01:51 +0600

    create admin and doctor

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -2,6 +2,8 @@ import express, { NextFunction, Request, Response } from "express";
 import { userController } from "./user.controller";
 import { fileUploader } from "../../helper/fileUploader";
 import { UserZodValidation } from "./user.validation";
+import { UserRole } from "@prisma/client";
+import auth from "../../middlewares/auth";
 
 const router = express.Router();
 
@@ -15,5 +17,29 @@ router.post(
     return userController.createPatient(req, res, next);
   }
 );
+
+router.post(
+    "/create-admin",
+    auth(UserRole.ADMIN),
+    fileUploader.upload.single('file'),
+    (req: Request, res: Response, next: NextFunction) => {
+        req.body = UserZodValidation.createAdminValidationSchema.parse(JSON.parse(req.body.data))
+        return userController.createAdmin(req, res, next)
+    }
+);
+
+router.post(
+    "/create-doctor",
+    auth(UserRole.ADMIN),
+    fileUploader.upload.single('file'),
+    (req: Request, res: Response, next: NextFunction) => {
+        console.log(JSON.parse(req.body.data))
+        req.body = UserZodValidation.createDoctorValidationSchema.parse(JSON.parse(req.body.data))
+        return userController.createDoctor(req, res, next)
+    }
+);
+
+
+
 
 export const userRoute = router;
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -4,6 +4,7 @@ import { prisma } from "../../shared/prisma";
 import { createPatientInput } from "./user.interface";
 import bcrypt from "bcryptjs";
 import { fileUploader } from "../../helper/fileUploader";
+import { UserRole } from "@prisma/client";
 
 const createPatient = async (req: Request) => {
   if (req.file) {
@@ -33,6 +34,71 @@ const createPatient = async (req: Request) => {
   return result;
 };
 
+const createAdmin = async (req: Request) => {
+  const file = req.file;
+
+  if (file) {
+    const uploadToCloudinary = await fileUploader.uploadToCloudinary(file);
+    req.body.admin.profilePhoto = uploadToCloudinary?.secure_url;
+  }
+
+  const hashedPassword: string = await bcrypt.hash(req.body.password, 10);
+
+  const userData = {
+    email: req.body.admin.email,
+    password: hashedPassword,
+    name: req.body.admin.name,
+    role: UserRole.ADMIN,
+  };
+
+  const result = await prisma.$transaction(async (transactionClient) => {
+    await transactionClient.user.create({
+      data: userData,
+    });
+
+    const createdAdminData = await transactionClient.admin.create({
+      data: req.body.admin,
+    });
+
+    return createdAdminData;
+  });
+
+  return result;
+};
+
+const createDoctor = async (req: Request) => {
+  const file = req.file;
+
+  if (file) {
+    const uploadToCloudinary = await fileUploader.uploadToCloudinary(file);
+    req.body.doctor.profilePhoto = uploadToCloudinary?.secure_url;
+  }
+  const hashedPassword: string = await bcrypt.hash(req.body.password, 10);
+
+  const userData = {
+    name: req.body.doctor.name,
+    email: req.body.doctor.email,
+    password: hashedPassword,
+    role: UserRole.DOCTOR,
+  };
+
+  const result = await prisma.$transaction(async (transactionClient) => {
+    await transactionClient.user.create({
+      data: userData,
+    });
+
+    const createdDoctorData = await transactionClient.doctor.create({
+      data: req.body.doctor,
+    });
+
+    return createdDoctorData;
+  });
+
+  return result;
+};
+
 export const UserService = {
   createPatient,
+  createAdmin,
+  createDoctor,
 };
