f4b9fb54bd08e190497fdcfbd88a6c42537ce5bd
nahidn228 <nahidn228@gmail.com>
2025-10-17 19:24:09 +0600

    convert startDateTime and endDateTime from boday using date-fns

---
+++ src/app/modules/schedule/schedule.controller.ts
@@ -0,0 +1,20 @@
+import  httpStatus  from 'http-status';
+import catchAsync from "../../shared/catchAsync";
+import sendResponse from "../../shared/sendResponse";
+import { ScheduleService } from './schedule.service';
+import { Request, Response } from 'express';
+
+const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
+    const result = await ScheduleService.insertIntoDB(req.body);
+
+    sendResponse(res, {
+        statusCode: 201,
+        success: true,
+        message: "Schedule created successfully!",
+        data: result
+    })
+});
+
+export const ScheduleController = {
+ insertIntoDB,
+};
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { userRoute } from "../modules/user/user.route";
 import { authRoute } from "../modules/auth/auth.route";
+import { ScheduleRoutes } from "../modules/schedule/schedule.route";
 
 const router = express.Router();
 
@@ -12,6 +13,10 @@ const moduleRoutes = [
   {
     path: "/auth",
     route: authRoute,
+  },
+  {
+    path: "/schedule",
+    route: ScheduleRoutes,
   },
 ];
 
