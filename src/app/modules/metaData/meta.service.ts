1fe51f770a97be819885bc18048259289dcdd2d9
nahidn228 <nahidn228@gmail.com>
2025-10-31 21:04:04 +0600

    Fetching Dashboard Metadata

--- src/app/modules/metaData/meta.service.ts
+++ src/app/modules/metaData/meta.service.ts
@@ -8,7 +8,7 @@ const fetchDashboardMetaData = async (user: IJwtPayload) => {
   let metaData;
   switch (user.role) {
     case UserRole.ADMIN:
-      metaData = "Admin MetaData";
+      metaData = await getAdminMetaData();
       break;
     case UserRole.DOCTOR:
       metaData = "Doctor MetaData";
@@ -24,51 +24,68 @@ const fetchDashboardMetaData = async (user: IJwtPayload) => {
   return metaData;
 };
 
-
-
 const getAdminMetaData = async () => {
-    const patientCount = await prisma.patient.count();
-    const doctorCount = await prisma.doctor.count();
-    const adminCount = await prisma.admin.count();
-    const appointmentCount = await prisma.appointment.count()
-    const paymentCount = await prisma.payment.count()
+  const patientCount = await prisma.patient.count();
+  const doctorCount = await prisma.doctor.count();
+  const adminCount = await prisma.admin.count();
+  const appointmentCount = await prisma.appointment.count();
+  const paymentCount = await prisma.payment.count();
 
-    const totalRevenue = await prisma.payment.aggregate({
-        _sum: {
-            amount: true
-        },
-        where: {
-            status: PaymentStatus.PAID
-        }
-    })
+  const totalRevenue = await prisma.payment.aggregate({
+    _sum: {
+      amount: true,
+    },
+    where: {
+      status: PaymentStatus.PAID,
+    },
+  });
 
-    const barChartData = await getBarChartData();
-    const pieChartData = await getPieChartData();
+  const barChartData = await getBarChartData();
+  const pieChartData = await getPieChartData();
 
-    return {
-        patientCount,
-        doctorCount,
-        adminCount,
-        appointmentCount,
-        paymentCount,
-        totalRevenue,
-        barChartData,
-        pieChartData
-    }
+  return {
+    patientCount,
+    doctorCount,
+    adminCount,
+    appointmentCount,
+    paymentCount,
+    totalRevenue,
+    barChartData,
+    pieChartData,
+  };
+};
 
-}
+const getBarChartData = async () => {
+  const appointmentCountPerMonth = await prisma.$queryRaw`
+        SELECT DATE_TRUNC('month', "createdAt") AS month,
+        CAST(COUNT(*) AS INTEGER) AS count
+        FROM "appointments"
+        GROUP BY month
+        ORDER BY month ASC
+    `;
 
+  return appointmentCountPerMonth;
+};
 
+const getPieChartData = async () => {
+  const appointmentStatusDistribution = await prisma.appointment.groupBy({
+    by: ["status"],
+    _count: { id: true },
+  });
 
+  const formatedAppointmentStatusDistribution =
+    appointmentStatusDistribution.map(({ status, _count }) => ({
+      status,
+      count: Number(_count.id),
+    }));
 
-
-
-
-
+  return formatedAppointmentStatusDistribution;
+};
 
 
 
 
 export const MetaService = {
   fetchDashboardMetaData,
+  getAdminMetaData,
 };
